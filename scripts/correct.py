523d3fa3cdd5a10f50f49f446c5fe7fc2fb36e6e
Wellington <wellington.71319@gmail.com>
2021-10-30 14:24:30 -0300

    Test scrpt

--- scripts/correct.py
+++ scripts/correct.py
@@ -1,13 +1,16 @@
 from conllu import parse_incr
 from io import open
 
-data_file = open("pt_bosque-ud-dev.conllu", "r", encoding="utf-8")
+data_file = open("pt_pud-ud-test.conllu", "r", encoding="utf-8")
 
-with open("dev_without_features.conllu", "w") as f:
+with open("test_update.conllu", "w") as f:
     for token_list in parse_incr(data_file):
         for token in token_list:
-            if (token['form'] == 'de'):
-                print(token['deprel'])
+            if ((token['form'] == 'a' or 
+                 token['form'] == 'o' or 
+                 token['form'] == 'os' or 
+                 token['form'] == 'as') and token['upos'] == 'DET'):
+                token['feats']['PronType'] = 'Art'                
         serialized = token_list.serialize()
         f.write(serialized)
         f.write('\n\n')
