790fcd295f21259535bda5fcec156f8c42446c52
Wellington <wellington.71319@gmail.com>
2021-10-31 23:18:31 -0300

    Test script

--- scripts/correct.py
+++ scripts/correct.py
@@ -1,17 +1,17 @@
 from conllu import parse_incr
+from conllu import parse_tree_incr
 from io import open
 
 list_def_det = ['a', 'as', 'o', 'os', 'A', 'As', 'O', 'Os']
 
-data_file = open("test.conllu", "r", encoding="utf-8")
+data_file = open("pt_pud-ud-test.conllu", "r", encoding="utf-8")
 
 with open("test_of_test.conllu", "w") as f:
     for token_list in parse_incr(data_file):
         for token in token_list:
             if ((token['form'] in list_def_det) and 
                 token['upos'] == 'DET' and 
-                token['deprel'] == 'det'and
-                token_list[token['head'] - 1]['upos'] == 'NOUN'):
+                token['deprel'] == 'det'):
 
                 token['feats']['PronType'] = 'Art'             
         serialized = token_list.serialize()
