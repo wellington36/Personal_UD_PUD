25319ca1aea220eb309e3a972d1d55487f3419f9
Wellington <wellington.71319@gmail.com>
2021-11-03 17:18:22 -0300

    Update scripts

--- scripts/correct.py
+++ scripts/correct.py
@@ -1,12 +1,11 @@
 from conllu import parse_incr
-from conllu import parse_tree_incr
 from io import open
 
 list_def_det = ['a', 'as', 'o', 'os', 'A', 'As', 'O', 'Os']
 
 data_file = open("pt_pud-ud-test.conllu", "r", encoding="utf-8")
 
-with open("test_of_test.conllu", "w") as f:
+with open("test.conllu", "w") as f:
     for token_list in parse_incr(data_file):
         for token in token_list:
             if ((token['form'] in list_def_det) and 
@@ -17,4 +16,3 @@ with open("test_of_test.conllu", "w") as f:
                 token['feats']['PronType'] = 'Art'             
         serialized = token_list.serialize()
         f.write(serialized)
-        f.write('\n\n')
--- scripts/issue_51.py
+++ scripts/issue_51.py
@@ -8,8 +8,11 @@ data_file = open("pt_pud-ud-test.conllu", "r", encoding="utf-8")
 with open("test.conllu", "w") as f:
     for token_list in parse_incr(data_file):
         for token in token_list:
-            if (token['form'] in list_def_det and token['upos'] == 'DET'):
-                token['feats']['PronType'] = 'Art'                
+            if ((token['form'] in list_def_det) and 
+                token['upos'] == 'DET' and 
+                token['deprel'] == 'det' and
+                any([i['upos'] in ['NOUN', 'PROPN'] for i in token_list if i['id'] == token['head']])):
+
+                token['feats']['PronType'] = 'Art'             
         serialized = token_list.serialize()
         f.write(serialized)
-        f.write('\n\n')
