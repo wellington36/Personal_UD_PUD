d5a6f1bb90769a5498c652f34ec9a9c145316628
Wellington <wellington.71319@gmail.com>
2021-11-05 10:56:55 -0300

    Update script

--- scripts/correct.py
+++ scripts/correct.py
@@ -1,18 +1,27 @@
 from conllu import parse_incr
 from io import open
+import sys
 
 list_def_det = ['a', 'as', 'o', 'os', 'A', 'As', 'O', 'Os']
 
-data_file = open("pt_pud-ud-test.conllu", "r", encoding="utf-8")
+def correctDet(file):
 
-with open("pt_pud-ud-test.conllu", "r+") as f: 
-    for token_list in parse_incr(data_file): 
-        for token in token_list:
-            if ((token['form'] in list_def_det) and 
-                token['upos'] == 'DET' and 
-                token['deprel'] == 'det' and
-                any([i['upos'] in ['NOUN', 'PROPN'] for i in token_list if i['id'] == token['head']])):
+    data_file = open(file, "r", encoding="utf-8")
+
+    with open(file, "r+") as f: 
+        for token_list in parse_incr(data_file): 
+            for token in token_list:
+                if ((token['form'] in list_def_det) and 
+                    token['upos'] == 'DET' and 
+                    token['deprel'] == 'det' and
+                    any([i['upos'] in ['NOUN', 'PROPN'] for i in token_list if i['id'] == token['head']])):
                 
-                token['feats']['PronType'] = 'Art'             
-        serialized = token_list.serialize()
-        f.write(serialized)
+                    if token['feats'] == None:
+                        token['feats'] = {'PronType': 'Art'}
+                    else:
+                        token['feats']['PronType'] = 'Art'           
+            serialized = token_list.serialize()
+            f.write(serialized)
+
+for conll in sys.argv[1:]:
+    correctDet(conll)
